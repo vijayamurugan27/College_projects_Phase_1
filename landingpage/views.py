200585b9a77c31dac8f0a0c0e378afe269fc8b8f
vijayamurugan27 <vijayamurugan27@gmail.com>
2022-12-14 08:13:33 +0530

    landing page changes

--- landingpage/views.py
+++ landingpage/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'landingpage/home.html')
+    return render(request, 'landingpage/resume.html')
 
 def resume(request):
     return render(request, 'landingpage/resume.html')
